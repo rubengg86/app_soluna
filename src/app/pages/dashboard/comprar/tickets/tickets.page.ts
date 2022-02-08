b30fb2bd86922605ce63b6e0bf90cf324a8b8f79
antonioff95 <antonioflorentinofernandez@gmail.com>
2022-02-08 12:34:23 +0100

    icono y tamaño proximamente

--- src/app/pages/dashboard/comprar/tickets/tickets.page.ts
+++ src/app/pages/dashboard/comprar/tickets/tickets.page.ts
@@ -41,7 +41,17 @@ export class TicketsPage implements OnInit {
     allowInput: true,
     enableTime: false,
     mode: 'single',
-    dateFormat: 'Y-m-d',
+    dateFormat: 'Y-m-d',locale: {
+      firstDayOfWeek: 1,
+      weekdays: {
+        shorthand: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+        longhand: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+      },
+      months: {
+      shorthand: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+      longhand: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+      }
+    },
     // this:
     enable: [{ from: new Date(0, 1), to: new Date(new Date().getFullYear() + 200, 12) }]
   };
